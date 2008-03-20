994d885a2be711bb0814f129bb9311d89d139722
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2008-03-20 15:20:17 +0000

    See ChangeLog
    git-svn-id: svn+ssh://io/home/lab8/igarashi/svn/books/CoPL/trunk/tools@1474 2a18b5b4-cca6-455a-ac5f-0e7f62b1625c

---
+++ examples/nat/pp.ml
@@ -0,0 +1,33 @@
+open Core
+open Format
+
+let pr = fprintf
+
+let (<) e1 e2 = match e1, e2 with
+    P(_,_), M(_,_) -> true
+  | _ -> false
+ 
+let with_paren ppf_e e_up ppf e =
+  if e < e_up then pr ppf "(%a)" ppf_e e else pr ppf "%a" ppf_e e
+
+let rec print_nat ppf = function
+    Z -> pr ppf "Z"
+  | S n -> pr ppf "S(%a)" print_nat n
+
+let rec print_exp ppf e = match e with
+    Exp_of_Nat n -> print_nat ppf n
+  | P (e1, e2) -> 
+      pr ppf "%a + %a" 
+	(with_paren print_exp e) e1 
+	(with_paren print_exp e) e2
+  | M (e1, e2) -> 
+      pr ppf "%a * %a" 
+	(with_paren print_exp e) e1 
+	(with_paren print_exp e) e2
+
+let print_judgment ppf = function
+    EvalTo (e, n) -> pr ppf "%a evalto %a" print_exp e print_nat n
+  | PlusIs (n1, n2, n3) -> 
+      pr ppf "%a plus %a is %a" print_nat n1 print_nat n2 print_nat n3
+  | MultIs (n1, n2, n3) ->
+      pr ppf "%a mult %a is %a" print_nat n1 print_nat n2 print_nat n3
