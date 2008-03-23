985e6fd1fbef2e7acd8f6b79964486b0a1d1f9ff
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2008-03-23 14:35:26 +0000

    Implemented function to output derivations in TeX. (Same to be done for ML1)
    git-svn-id: svn+ssh://io/home/lab8/igarashi/svn/books/CoPL/trunk/tools@1496 2a18b5b4-cca6-455a-ac5f-0e7f62b1625c

--- examples/common/main.ml
+++ examples/common/main.ml
@@ -2,9 +2,11 @@ open Lexing
 
 let filename = ref ""
 
-let mode = ref false  (* controls whether full derivations are shown *)
+let fullp = ref false  (* controls whether full derivations are shown *)
+let texp = ref false   (* controls whether output is in TeX *)
 
-let spec = [("-full", Arg.Set mode, "Display full derivations");]
+let spec = [("-full", Arg.Set fullp, "Display full derivations");
+	    ("-TeX", Arg.Set texp, "output in TeX")]
 
 let () = 
   Arg.parse spec (fun s -> filename := s) "Usage: [-full] filename";
@@ -16,11 +18,16 @@ let () =
   while true do
     let d = Parser.toplevel Lexer.main lexbuf in
     let j = Core.deriv_check d in
-      if !mode then
-	Syntax.print_deriv Pp.print_judgment Format.std_formatter d
-      else
-	Pp.print_judgment Format.std_formatter d.Syntax.conc;
-      Format.print_newline();
+      (match !fullp, !texp with
+	  true, true -> 
+	    Syntax.tex_deriv Pp.tex_judgment Format.std_formatter d
+	| true, false -> 
+	    Syntax.print_deriv Pp.print_judgment Format.std_formatter d
+	| false, true ->
+	    Pp.tex_judgment Format.std_formatter d.Syntax.conc
+	| false, false ->
+	    Pp.print_judgment Format.std_formatter d.Syntax.conc);
+      Format.print_newline()
   done
 
 
