d3a830fae877daa131e13e3011b255446f27fa3a
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2008-03-29 14:59:19 +0000

    prover generation.
    git-svn-id: svn+ssh://io/home/lab8/igarashi/svn/books/CoPL/trunk/tools@1522 2a18b5b4-cca6-455a-ac5f-0e7f62b1625c

--- examples/common/main.ml
+++ examples/common/main.ml
@@ -4,6 +4,7 @@ let commandname = "checker"
 
 let filename = ref ""  (* the name of the input file *)
 let gname = ref ""     (* the name of a game *)
+let jdg = ref ""       (* the judgment to prove *)
 
 let fullp = ref false  (* controls whether full derivations are shown *)
 let texp = ref false   (* controls whether output is in TeX *)
@@ -12,30 +13,38 @@ let spec = [
     ("-full", Arg.Set fullp, "Display full derivations");
     ("-TeX", Arg.Set texp, "output in TeX");
     ("-game", Arg.Set_string gname, "the name of a game");
+    ("-prove", Arg.Set_string jdg, "the judgment to prove");
   ]
 
 let games = [
-    ("nat", Nat.Link.process_input);
-    ("ML1", Ml1.Link.process_input);
+    ("nat", (Nat.Link.process_derivation, Nat.Link.process_partial_jdg));
+(*    ("ML1", Ml1.Link.process_derivation); *)
   ]
 
 let () = 
   Arg.parse spec (fun s -> filename := s) 
-    (Printf.sprintf "Usage: %s -game gamename [-full] [-TeX] filename" 
-	commandname);
+    (Printf.sprintf "Usage: %s -game gamename [-full] [-TeX] filename\n%s -game gamename -prove judgment" 
+	commandname commandname);
 
   if !gname = "" then failwith "Game name must be given"
   else
     begin
-      let process_f = 
+      let process_deriv, process_partial_jdg = 
 	try List.assoc !gname games with 
 	    Not_found -> failwith ("No such game: " ^ !gname) in
-      let lexbuf = Lexing.from_channel (open_in !filename) in
-
-      let pos = lexbuf.lex_curr_p in
-	lexbuf.lex_curr_p <- { pos with pos_fname = !filename };
-
-	while true do
-	  process_f lexbuf !fullp !texp;
-	done
+	if !jdg = "" then (* checker mode *)
+	  begin
+	    let lexbuf = Lexing.from_channel (open_in !filename) in
+	    
+	    let pos = lexbuf.lex_curr_p in
+	      lexbuf.lex_curr_p <- { pos with pos_fname = !filename };
+	      
+	      while true do
+		process_deriv lexbuf !fullp !texp;
+	      done
+	  end
+	    
+	else (* -prove mode*)
+	  let lexbuf = Lexing.from_string !jdg in
+	    process_partial_jdg lexbuf !fullp !texp
     end
