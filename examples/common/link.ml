4e04e65a8d37b6c5904215c42b49c7ae2fc39e22
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2008-03-29 14:59:18 +0000

    prover generation.
    git-svn-id: svn+ssh://io/home/lab8/igarashi/svn/books/CoPL/trunk/tools@1521 2a18b5b4-cca6-455a-ac5f-0e7f62b1625c

--- examples/common/link.ml
+++ examples/common/link.ml
@@ -17,15 +17,30 @@ module Lexer = Lexer.Make(
 	and lbrace = LBRACE
 	and rparen = RPAREN
 	and lparen = LPAREN
+	and qm = QM
       end 
 
       module K = Keywords
     end
   )
 
-let process_input lexbuf fullp texp =
+let process_derivation lexbuf fullp texp =
   let d = Parser.toplevel Lexer.main lexbuf in
   let j = Checker.deriv_check d in
+    (match fullp, texp with
+	true, true -> 
+	  Derivation.tex_deriv Pp.tex_judgment Format.std_formatter d
+      | true, false -> 
+	  Derivation.print_deriv Pp.print_judgment Format.std_formatter d
+      | false, true ->
+	  Pp.tex_judgment Format.std_formatter d.Derivation.conc
+      | false, false ->
+	  Pp.print_judgment Format.std_formatter d.Derivation.conc);
+    Format.print_newline()
+
+let process_partial_jdg lexbuf fullp texp =
+  let j = Parser.partialj Lexer.main lexbuf in
+  let d = Checker.make_deriv j in
     (match fullp, texp with
 	true, true -> 
 	  Derivation.tex_deriv Pp.tex_judgment Format.std_formatter d
