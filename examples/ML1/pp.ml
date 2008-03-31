3f45d89fe4a39c49d40777043a0cb1d8300a48fc
Atsushi Igarashi <igarashi@kuis.kyoto-u.ac.jp>
2008-03-31 14:18:20 +0000

    see ChangeLog
    git-svn-id: svn+ssh://io/home/lab8/igarashi/svn/books/CoPL/trunk/tools@1544 2a18b5b4-cca6-455a-ac5f-0e7f62b1625c

--- examples/ML1/pp.ml
+++ examples/ML1/pp.ml
@@ -61,6 +61,14 @@ let print_judgment ppf = function
       let op = match p with Plus -> "plus" | Minus -> "minus" | Mult -> "mult"
       in pr ppf "%a %s %a is %a" print_val v1 op print_val v2 print_val v3
 
+let print_pjudgment ppf = function
+    In_EvalTo e -> pr ppf "%a evalto ?" print_exp e
+  | In_AppBOp (Lt, v1, v2) ->
+      pr ppf "%a is less than %a ?" print_val v1 print_val v2
+  | In_AppBOp (p, v1, v2) -> 
+      let op = match p with Plus -> "plus" | Minus -> "minus" | Mult -> "mult"
+      in pr ppf "%a %s %a is ?" print_val v1 op print_val v2
+
 let tex_judgment ppf = function
     EvalTo (e, v) -> pr ppf "\\EvalTo{%a}{%a}" print_exp e print_val v
   | AppBOp (p, v1, v2, v3) -> 
